bbf772713adfd04332d963e25e7d862a6dfe49e2
leoschuhmann <schuhmann.leo@gmx.de>
2023-04-07 16:54:50 +0200

    .

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,6 @@
 # Importing required functions
 import pandas
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from fileinput import filename
 
 from Processing.extract_kpis_from_hcm import create_kpis as create_kpis_hcm
@@ -25,11 +25,13 @@ def view():
 	# Read the File using Flask request
 	# file = request.files
 	files = request.files.getlist("file")
+	company_name = request.form['company']
+	print(company_name)
 	for file in files:
 		file.save("Flask/data/" + file.filename)
 	# save file in local directory
 	# file.save("Flask/data/" + file.filename)
-
+	
 	data = pandas.read_csv("Flask/data/SAP_HCM.csv")
 	kpi_hcm = create_kpis_hcm(data)
 	print(kpi_hcm)
@@ -68,19 +70,20 @@ def view():
 
 	# Return HTML snippet that will render the table
 	# find_risks()
-	return render_template('success.html')
+	return redirect(url_for('risks'))
 # render_template('show_tables.html',  tables=[hcm_risks_db.to_html(classes='data'), data.to_html(classes='data')])
 
 @app.get('/risks')
 def risks():
-	hcm=pandas.read_excel("Flask/risks/HCM_risks.xlsx")
-	pur=pandas.read_excel("Flask/risks/PUR_risks.xlsx")
-	prod=pandas.read_excel("Flask/risks/PROD_risks.xlsx")
+	hcm=pandas.read_excel("Flask/risks/HCM_risks.xlsx").rename(columns={"Unnamed: 0": "risk", 0:"value"})
+	pur=pandas.read_excel("Flask/risks/PUR_risks.xlsx").rename(columns={"Unnamed: 0": "risk", 0:"value"})
+	prod=pandas.read_excel("Flask/risks/PROD_risks.xlsx").rename(columns={"Unnamed: 0": "risk", 0:"value"})
 
-	return render_template('show_tables.html',  tables=[hcm.to_html(classes='data'), 
-					      pur.to_html(classes='data'),
-					      prod.to_html(classes='data')			  
-						  ])
+	df2 = pandas.concat([hcm, pur], axis=0)
+	df3 = pandas.concat([df2, prod], axis=0).reset_index().drop('index', axis=1)
+
+
+	return render_template('show_tables.html',  tables=[df3.to_html(classes='data')])
 
 
 # Main Driver Function
